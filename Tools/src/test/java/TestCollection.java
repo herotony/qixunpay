f0a9781763b5925097f437d3e88d5ce14f6a834b
herotony <herotony@gmail.com>
2017-09-20 11:12:43 +0800

    追加dubbo的简版hessian lite使用的intmap，该intMap用来计算某个方法或url的引用次数,因其value只存int型

---
+++ Tools/src/test/java/TestCollection.java
@@ -0,0 +1,28 @@
+import com.qixunpay.Tools.Collection.IntMap;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by saosinwork on 2017/9/20.
+ */
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+public class TestCollection {
+
+    @Test
+    public void testInitMap(){
+
+        int testMask = 3677 % 1000;
+
+        IntMap map = new IntMap();
+        map.put("some.f",0);
+        map.put("some.t",0);
+        int result = map.get("some.k");
+        result = map.get("some.t");
+    }
+
+}
